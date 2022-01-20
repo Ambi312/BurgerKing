702cc152b5a07d74cba5b7de299e7f510a41bdde
Ambi <abd.kanybek@gmail.com>
2022-01-20 17:07:19 +0600

    CRUD, cart

--- burger_king/settings.py
+++ burger_king/settings.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # my lips
+    'cart',
+
     # my apps
     'menu',
 ]
@@ -56,11 +59,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processor.cart_total_amount',
             ],
         },
     },
 ]
 
+CART_SESSION_ID = 'cart'
 WSGI_APPLICATION = 'burger_king.wsgi.application'
 
 
@@ -121,6 +126,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
